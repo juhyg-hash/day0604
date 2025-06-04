451c9faa57fe8a3b7ae70c36f88b5fa9e55b7c2d
juhyg-hash <gusrb1474@gmail.com>
2025-06-04 11:30:19 +0900

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 7;
+        for(int i = 10; i >=1; i--){
+            System.out.println(i);
+        }
 
-        System.out.println("a == b : " + (a == b));
-        System.out.println("a != b : "+ (a != b));
 
-        System.out.println("a > b : " + (a>b));
-        System.out.println("a < b : " + (a<b));
-        System.out.println("a >= b : " + (a>=b));
-        System.out.println("a <= b : " + (a<=b));
+        int num = 5;
+        for(int i =1; i <= 9; i++){
+            System.out.println(num + " x " + i +"=" + (num*i));
+        }
+
+        int num1 = 19;
+        for(int i = 19; i>= 1; i--){
+            System.out.println(num1 + " x " + i +"=" + (num1*i));
+        }
+
+        int num2 = 6;
+        for(int i =1; i<=6; i++){
+            System.out.println(num2 + "*" + i + "="+ (num2 * i ));
+        }
+
     }
 }
