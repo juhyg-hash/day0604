6f3612309db27892d88f991de8bd8d16ce19a875
juhyg-hash <gusrb1474@gmail.com>
2025-06-04 15:08:32 +0900

    eigth commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        boolean a= true;
-        int b = 1 ;
+        int input = 5;
 
-        while (a) {
-            System.out.println(b);
-            b++;
+        do{
+            System.out.println("자판기 입니다");
 
-            if(b> 10000){
-                a = false;
+            //키입력
+
+            switch(input){
+                case 1 :
+                    System.out.println("콜라");
+                    break;
+                case 2 :
+                    System.out.println("사이다");
+                    break;
+                case 3 :
+                    System.out.println("물");
+                    break;
+                default:
+                    System.out.println("잘못 고름");
             }
-        }
 
-    }
+//            input = 0;
+
+        } //while(input != 0);
+            while(input == 0);
+
+       }
 }
