d077210ab7e7e4777cbe5606a4fb5f14becb4631
juhyg-hash <gusrb1474@gmail.com>
2025-06-04 14:28:34 +0900

    sixth commit

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-//        for (int i = 1; i <= 9; i++) {
-//            for (int j = 1; j <= 9; j ++) {
-//                System.out.println();
-//            }
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(i);
 //        }
 
-//
-//        for (int i = 1; i <= 9; i++) {
-//            // <--j의 반복이 끝난후 i 의 반복 시작
-//            for (int j = 1; j <= 9; j ++) {
-//                System.out.println("i는 " + i + ", j는 " + j);
-//            }
-//            // <-- j의 반복이시작 딜때 j의 반복을 하고 다시 i 두번째 반복으로 넘어가면 다시 j의 반복
-//        }
-        for (int i = 1; i <= 9; i++) {
-            System.out.println(i+"단");
-            for (int j = 1; j <= 9; j ++) {
-                System.out.println(i + " x " + j + " = " + (i * j));
+        int i = 0;
 
-                
-            }
-
+        while (i < 5) {
+            System.out.println(i);
+            i++;
         }
+
+//        do{
+//            System.out.println(i);
+//            i++;
+//        } while(i < 5);
     }
-}
+
+    }
