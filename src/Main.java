e966babb9cee8028979e528dff346c09b6d16f01
juhyg-hash <gusrb1474@gmail.com>
2025-06-04 11:48:18 +0900

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i = 10; i >=1; i--){
-            System.out.println(i);
-        }
+//      for(int i =1; i<= 10; i++){
+//          if(i %2 == 0){
+//              System.out.println("i =" + i);
+//          }
 
-
-        int num = 5;
-        for(int i =1; i <= 9; i++){
-            System.out.println(num + " x " + i +"=" + (num*i));
-        }
-
-        int num1 = 19;
-        for(int i = 19; i>= 1; i--){
-            System.out.println(num1 + " x " + i +"=" + (num1*i));
-        }
-
-        int num2 = 6;
-        for(int i =1; i<=6; i++){
-            System.out.println(num2 + "*" + i + "="+ (num2 * i ));
+        for(int i= 20; i>=1; i--){
+            if(i % 4 ==0){
+                System.out.println(i);
+            }
         }
 
     }
